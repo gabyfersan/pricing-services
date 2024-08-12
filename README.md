7ee2ed9cda365122e9e066fd90d42ef160356722
Gaby Fersan <gabyfersan@Gabys-iMac.local>
2024-08-12 17:32:31 +0200

    Add ssl

--- README.md
+++ README.md
@@ -12,9 +12,14 @@ To run:
 bun run index.ts
 ```
 
+curl -fsSL https://bun.sh/install | bash
+
+exec /bin/zsh
+bun run src/server.ts
+
 This project was created using `bun init` in bun v1.1.17. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
 
 The API inludes all days e.i the first and last days
 
-http://localhost:8000/price?customerId=X&startDate=2019-09-20&endDate=2019-10-01
-http://localhost:3000/price?customerId=X&start=2019-09-20&end=2019-10-01
+https://localhost:443/price?customerId=X&startDate=2019-09-20&endDate=2019-10-01
+https://localhost:443/price?customerId=Y&startDate=2018-01-01&endDate=2019-10-01
--- src/server.ts
+++ src/server.ts
@@ -1,62 +1,64 @@
+import { serve } from 'bun';
 import { validateInputs } from '../utils/utils';
 import { doesCustomerExist } from './customer';
 import { calculatePrice } from './pricing';
 import { PriceCalculationRequest } from './types';
 
-// const options = {
-//   certFile: '../certification/cert.pem', // Path to your certificate file
-//   keyFile: '../certification/key.pem', // Path to your private key file
-//   port: 443, // Default HTTPS port
-// };
+const sslOptions = {
+  certFile: 'certification/cert.pem', // Path to your certificate file
+  keyFile: 'certification/key.pem', // Path to your private key file
+  port: 443, // Default HTTPS port
+};
 
-// const server = Deno.L({
-//   hostname: '0.0.0.0', // Listen on all available interfaces
-//   port: options.port,
-//   certFile: options.certFile,
-//   keyFile: options.keyFile,
-// });
+const server = serve({
+  port: sslOptions.port,
+  tls: {
+    cert: Bun.file(sslOptions.certFile),
+    key: Bun.file(sslOptions.keyFile),
+  },
+  async fetch(req) {
+    const url = new URL(req.url);
 
-//console.log(`HTTPS web server running on https://localhost:${options.port}/`);
+    if (url.pathname === '/price' && req.method === 'GET') {
+      const customerId = url.searchParams.get('customerId') ?? '';
+      const startDate = url.searchParams.get('startDate') ?? '';
+      const endDate = url.searchParams.get('endDate') ?? '';
 
-// for await (const conn of server) {
-//   serve(handler, { conn, httpConn: Deno.serveHttp(conn) });
-// }
-Deno.serve(async (req: Request) => {
-  const url = new URL(req.url);
+      const request: PriceCalculationRequest = {
+        customerId,
+        startDate,
+        endDate,
+      };
 
-  if (url.pathname === '/price' && req.method === 'GET') {
-    const customerId = url.searchParams.get('customerId') ?? '';
-    const startDate = url.searchParams.get('startDate') ?? '';
-    const endDate = url.searchParams.get('endDate') ?? '';
+      if (!validateInputs(request)) {
+        return new Response(JSON.stringify({ error: 'Invalid input' }), {
+          status: 400,
+        });
+      }
 
-    const request: PriceCalculationRequest = {
-      customerId,
-      startDate,
-      endDate,
-    };
-    if (!validateInputs(request)) {
-      return new Response(JSON.stringify({ error: 'Invalid input' }), {
-        status: 400,
-      });
+      if (!doesCustomerExist(customerId)) {
+        return new Response(JSON.stringify({ error: 'Customer Not Found' }), {
+          status: 401,
+        });
+      }
+
+      try {
+        const price = await calculatePrice(request.customerId, request.startDate, request.endDate);
+        return new Response(JSON.stringify({ price }), { status: 200 });
+      } catch (error) {
+        return new Response(
+          JSON.stringify({
+            error: error instanceof Error ? error.message : 'An unknown error occurred',
+          }),
+          { status: 500 },
+        );
+      }
     }
-
-    if (!doesCustomerExist(customerId)) {
-      return new Response(JSON.stringify({ error: 'Customer Not Found' }), {
-        status: 401,
-      });
-    }
-
-    try {
-      const price = await calculatePrice(request.customerId, request.startDate, request.endDate);
-      return new Response(JSON.stringify({ price }), { status: 200 });
-    } catch (error) {
-      return new Response(
-        JSON.stringify({
-          error: error instanceof Error ? error.message : 'An unknown error occurred',
-        }),
-        { status: 500 },
-      );
-    }
-  }
-  return new Response('Not Found ' + url, { status: 404 });
+    return new Response('Not Found ' + url, { status: 404 });
+  },
 });
+console.log(`Listening on https://localhost:${server.port} ...`);
+console.log(`Please use `);
+console.log(`
+https://localhost:443/price?customerId=X&startDate=2019-09-20&endDate=2019-10-01
+https://localhost:443/price?customerId=Y&startDate=2018-01-01&endDate=2019-10-01`);
